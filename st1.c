d92b3c979f3328bf94e85782c142da20a2292e81
Hurtswark <guilhermequitete19@gmail.com>
2026-03-30 10:35:50 -0300

    Add files via upload

---
+++ st1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct{
+
+int dia,mes,ano;
+
+}data;
+
+int main(){
+
+data p1;
+
+printf("Digite o dia:");
+scanf("%d",&p1.dia);
+
+printf("Digite o mes:");
+scanf("%d",&p1.mes);
+
+printf("Digite o ano:");
+scanf("%d",&p1.ano);
+}
+
+
+
+
